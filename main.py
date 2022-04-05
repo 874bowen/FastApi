40049a689390bec7033029c95af89d99e69b36b5
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-05 07:43:43 +0300

    fastApi validation of post with BaseModel and Optional

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+# title str, content str, category, Bool published
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # giving it a default value
+    rating: Optional[int] = None  # fully optional field
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my API"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post.rating)
+    print(post.dict())
+    return {"data": post}
+
+# title str, content str, category, Bool published
