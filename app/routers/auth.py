c5330e3327dc58280a62c2a2a452ae66bf610a8a
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-14 21:38:06 +0300

    added response scheme for login 'response_model'

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,17 +10,17 @@ router = APIRouter(
 )
 
 
-@router.get("/login")
+@router.get("/login", response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     # OAuth2PasswordRequestForm is going to store email as username
     user = db.query(models.Users).filter(models.Users.email == user_credentials.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Invalid Credentials")
     if not utils.verify_password(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Invalid Credentials")
     # create a token
     access_token = oauth2.create_access_token(data={"user_id": user.id})
     # return token
-    return {"access token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer"}
