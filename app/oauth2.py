4f2f91abb307963ccc77a11c8b40e55278a92607
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-14 21:49:49 +0300

    verify access token and get current user

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,7 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException, Depends
 
 # SECRET KEY
 # openssl rand -hex 32
@@ -7,6 +9,12 @@ from datetime import datetime, timedelta
 # from base64 import b64encode
 #
 # print(b64encode(token_bytes(32)).decode())
+from starlette import status
+
+from app import schemas
+
+OAuth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 SECRET_KEY = "Svj/QuHTMLI42ywAHAS9MmX63mAFP8+0NUB0B5bmKcM="
 
 # Algorithm
@@ -21,3 +29,25 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        print(token)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError as e:
+        print(e)
+        raise credentials_exception
+
+    return token_data
+
+
+def get_current_user(token: str = Depends(OAuth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail="Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"})
+    return verify_access_token(token, credentials_exception)
