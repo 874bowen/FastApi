52f70bec97c6c6ee8a83563ce57d8eec92aa6729
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-09 21:40:17 +0300

    access token to expire after 30 minutes

--- app/oauth2.py
+++ app/oauth2.py
@@ -23,7 +23,7 @@ SECRET_KEY = "Svj/QuHTMLI42ywAHAS9MmX63mAFP8+0NUB0B5bmKcM="
 # Algorithm
 ALGORITHM = "HS256"
 # Expiration time
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def create_access_token(data: dict):
