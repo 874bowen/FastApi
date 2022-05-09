786457b6fd7b41af567fe648acb9b84afac48941
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-09 21:41:16 +0300

    using db to make queries for the current logged in user

--- app/oauth2.py
+++ app/oauth2.py
@@ -49,7 +49,7 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
 
 
-def get_current_user(token: str = Depends(OAuth2_scheme)):
+def get_current_user(token: str = Depends(OAuth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail="Could not validate credentials",
                                           headers={"WWW-Authenticate": "Bearer"})
