3d630d252004019adc657c5ba89c92f070697edc
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-09 21:41:45 +0300

    getting the current logged in user

--- app/oauth2.py
+++ app/oauth2.py
@@ -53,4 +53,6 @@ def get_current_user(token: str = Depends(OAuth2_scheme), db: Session = Depends(
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail="Could not validate credentials",
                                           headers={"WWW-Authenticate": "Bearer"})
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.Users).filter(models.Users.id == token.id).first()
+    return user
