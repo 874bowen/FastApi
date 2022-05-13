9b9963f1884f7db926c92a0373cbebf6423a4c5d
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-13 16:56:48 +0300

    foreign key added (owner_id)

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text, ForeignKey
 
 from .database import Base
 
@@ -10,6 +10,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="TRUE", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    owner_id= Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 
 class Users(Base):
